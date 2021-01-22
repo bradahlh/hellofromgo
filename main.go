0ef9802ac4f24e83cfea3af986f7cb69b9ce0a49
Andreas Bradahl <andreasbradahl@gmail.com>
2021-01-22 11:27:11 +0100

    Update functino

--- main.go
+++ main.go
@@ -4,13 +4,21 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello from main.go!")
+func AddHandler(response http.ResponseWriter, request *http.Request) {
+	queryParams := mux.Vars(request)
+	response.WriteHeader(http.StatusOK)
+	value1Int, _ := strconv.Atoi(queryParams["value1"])
+	value2Int, _ := strconv.Atoi(queryParams["value2"])
+	fmt.Fprintf(response, strconv.Itoa(value1Int+value2Int))
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8888", nil))
+	router := mux.NewRouter()
+	router.HandleFunc("/add/{value1}/{value2}", AddHandler)
+	log.Fatal(http.ListenAndServe(":8888", router))
 }
