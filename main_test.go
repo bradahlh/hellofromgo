e94b11dcf4bd711d0053b3804c964d8035ec0b48
Andreas Bradahl <andreasbradahl@gmail.com>
2021-01-22 11:27:21 +0100

    Add test file

---
+++ main_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/assert"
+)
+
+func Router() *mux.Router {
+	router := mux.NewRouter()
+	router.HandleFunc("/add/{value1}/{value2}", AddHandler)
+	return router
+}
+
+func TestRootEndpoint(t *testing.T) {
+	request, _ := http.NewRequest("GET", "/add/2/3", nil)
+	response := httptest.NewRecorder()
+	Router().ServeHTTP(response, request)
+	assert.Equal(t, 200, response.Code, "OK response is expected")
+	assert.Equal(t, "5", response.Body.String(), "Expect response '5'")
+}
